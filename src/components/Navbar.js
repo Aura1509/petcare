caa1c3bd84ccf70c5f632b74c263af624b09c114
Julian Palco <palcojulian@outlook.com>
2024-10-15 22:58:49 -0500

    Borra cache git, y se crea gitignore

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+// src/components/Navbar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav>
+          </nav>
+  );
+};
+
+export default Navbar;

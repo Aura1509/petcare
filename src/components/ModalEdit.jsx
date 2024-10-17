6ae521475e04816532ce4c6c2abefdff59b2882f
Julian Palco <palcojulian@outlook.com>
2024-10-16 23:59:26 -0500

    Modal para editar mascota

---
+++ src/components/ModalEdit.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../styles/modal.css'
+
+const Modal = ({ isOpen, onClose, children, titleModal }) => {
+    if (!isOpen) return null; // No renderiza el modal si no está abierto
+
+    return (
+        <div className="modal-overlay">
+            <div className="modal-content">
+                <div className='modal-header' >
+                    <h3>{titleModal}</h3>
+                    <button className="modal-close" onClick={onClose}>
+                        X
+                    </button>
+                </div>
+
+                {children} {/* Contenido dinámico */}
+            </div>
+        </div>
+    );
+};
+
+export default Modal;
